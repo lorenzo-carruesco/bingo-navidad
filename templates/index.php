b0a525c744b22957470feb20524d4e4ce350b3e6
lorenzo-carruesco <lorenzo.carruesco@gmail.com>
2016-12-22 00:42:55 +0100

    primer diseño básico

--- templates/index.php
+++ templates/index.php
@@ -1,8 +1,112 @@
 <html>
     <head>
-        
+        <link type="text/css" rel="stylesheet" href="/css/global.css">
     </head>
     <body>
+        <div id="container">
+            <div id="number-panel">
+                <div><img src="/img/n/1.png" alt="1" /></div>
+                <div><img src="/img/n/2.png" alt="2" /></div>
+                <div><img src="/img/n/3.png" alt="3" /></div>
+                <div><img src="/img/n/4.png" alt="4" /></div>
+                <div><img src="/img/n/5.png" alt="5" /></div>
+                <div><img src="/img/n/6.png" alt="6" /></div>
+                <div><img src="/img/n/7.png" alt="7" /></div>
+                <div><img src="/img/n/8.png" alt="8" /></div>
+                <div><img src="/img/n/9.png" alt="9" /></div>
+                <div><img src="/img/n/10.png" alt="10" /></div>
+                
+                <div><img src="/img/n/11.png" alt="11" /></div>
+                <div><img src="/img/n/12.png" alt="12" /></div>
+                <div><img src="/img/n/13.png" alt="13" /></div>
+                <div><img src="/img/n/14.png" alt="14" /></div>
+                <div><img src="/img/n/15.png" alt="15" /></div>
+                <div><img src="/img/n/16.png" alt="16" /></div>
+                <div><img src="/img/n/17.png" alt="17" /></div>
+                <div><img src="/img/n/18.png" alt="18" /></div>
+                <div><img src="/img/n/19.png" alt="19" /></div>
+                <div><img src="/img/n/20.png" alt="0" /></div>
+                
+                <div><img src="/img/n/21.png" alt="21" /></div>
+                <div><img src="/img/n/22.png" alt="22" /></div>
+                <div><img src="/img/n/23.png" alt="23" /></div>
+                <div><img src="/img/n/24.png" alt="24" /></div>
+                <div><img src="/img/n/25.png" alt="25" /></div>
+                <div><img src="/img/n/26.png" alt="26" /></div>
+                <div><img src="/img/n/27.png" alt="27" /></div>
+                <div><img src="/img/n/28.png" alt="28" /></div>
+                <div><img src="/img/n/29.png" alt="29" /></div>
+                <div><img src="/img/n/30.png" alt="30" /></div>
+                
+                <div><img src="/img/n/31.png" alt="31" /></div>
+                <div><img src="/img/n/32.png" alt="32" /></div>
+                <div><img src="/img/n/33.png" alt="33" /></div>
+                <div><img src="/img/n/34.png" alt="34" /></div>
+                <div><img src="/img/n/35.png" alt="35" /></div>
+                <div><img src="/img/n/36.png" alt="36" /></div>
+                <div><img src="/img/n/37.png" alt="37" /></div>
+                <div><img src="/img/n/38.png" alt="38" /></div>
+                <div><img src="/img/n/39.png" alt="39" /></div>
+                <div><img src="/img/n/40.png" alt="40" /></div>
+                
+                <div><img src="/img/n/41.png" alt="41" /></div>
+                <div><img src="/img/n/42.png" alt="42" /></div>
+                <div><img src="/img/n/43.png" alt="43" /></div>
+                <div><img src="/img/n/44.png" alt="44" /></div>
+                <div><img src="/img/n/45.png" alt="45" /></div>
+                <div><img src="/img/n/46.png" alt="46" /></div>
+                <div><img src="/img/n/47.png" alt="47" /></div>
+                <div><img src="/img/n/48.png" alt="48" /></div>
+                <div><img src="/img/n/49.png" alt="49" /></div>
+                <div><img src="/img/n/50.png" alt="50" /></div>
+                
+                <div><img src="/img/n/51.png" alt="51" /></div>
+                <div><img src="/img/n/52.png" alt="52" /></div>
+                <div><img src="/img/n/53.png" alt="53" /></div>
+                <div><img src="/img/n/54.png" alt="54" /></div>
+                <div><img src="/img/n/55.png" alt="55" /></div>
+                <div><img src="/img/n/56.png" alt="56" /></div>
+                <div><img src="/img/n/57.png" alt="57" /></div>
+                <div><img src="/img/n/58.png" alt="58" /></div>
+                <div><img src="/img/n/59.png" alt="59" /></div>
+                <div><img src="/img/n/60.png" alt="60" /></div>
+                
+                <div><img src="/img/n/61.png" alt="61" /></div>
+                <div><img src="/img/n/62.png" alt="62" /></div>
+                <div><img src="/img/n/63.png" alt="63" /></div>
+                <div><img src="/img/n/64.png" alt="64" /></div>
+                <div><img src="/img/n/65.png" alt="65" /></div>
+                <div><img src="/img/n/66.png" alt="66" /></div>
+                <div><img src="/img/n/67.png" alt="67" /></div>
+                <div><img src="/img/n/68.png" alt="68" /></div>
+                <div><img src="/img/n/69.png" alt="69" /></div>
+                <div><img src="/img/n/70.png" alt="70" /></div>
+                
+                <div><img src="/img/n/71.png" alt="71" /></div>
+                <div><img src="/img/n/72.png" alt="72" /></div>
+                <div><img src="/img/n/73.png" alt="73" /></div>
+                <div><img src="/img/n/74.png" alt="74" /></div>
+                <div><img src="/img/n/75.png" alt="75" /></div>
+                <div><img src="/img/n/76.png" alt="76" /></div>
+                <div><img src="/img/n/77.png" alt="77" /></div>
+                <div><img src="/img/n/78.png" alt="78" /></div>
+                <div><img src="/img/n/79.png" alt="79" /></div>
+                <div><img src="/img/n/80.png" alt="80" /></div>
+                
+                <div><img src="/img/n/81.png" alt="81" /></div>
+                <div><img src="/img/n/82.png" alt="82" /></div>
+                <div><img src="/img/n/83.png" alt="83" /></div>
+                <div><img src="/img/n/84.png" alt="84" /></div>
+                <div><img src="/img/n/85.png" alt="85" /></div>
+                <div><img src="/img/n/86.png" alt="86" /></div>
+                <div><img src="/img/n/87.png" alt="87" /></div>
+                <div><img src="/img/n/88.png" alt="88" /></div>
+                <div><img src="/img/n/89.png" alt="89" /></div>
+                <div><img src="/img/n/90.png" alt="90" /></div>
+                
+            </div>
+        </div>
         
+        <script type="text/javascript" src="/js/script.js" ></script>
     </body>
 </html>
