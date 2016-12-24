4ae0afd5e12fce37b3b1b4c183047af061503c5e
lorenzo-carruesco <lorenzo.carruesco@gmail.com>
2016-12-24 19:08:18 +0100

    simple behavior

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$("#new-game").click(function(e) {
+    return confirm("Nueva Partida");
+});
 
-
+$("#new-ball").click(function(e) {
+    e.preventDefault();
+    var remaining_numbers = $(".number:not(.glowed)");
+    var index = Math.floor((Math.random() * remaining_numbers.size())); 
+    var number = remaining_numbers.eq(index).text();
+    $("#ball-output .ball").fadeOut(300, function () {
+        $("#ball-output").html('<div class="ball" style="display:none">'+number+'</div>');
+        $("#ball-output .ball").fadeIn(300, function () {
+            $(".number#number-"+number).addClass('glowed');
+        });
+    });
+});
--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,7 @@
         <div id="container">
             <div id="number-panel">
                 <?php for ($i = 1; $i <= 90; $i++) {
-                    ?><div><span id="number-panel-<?= $i ?>" ><?= $i ?></span></div><?php }
+                    ?><div><span class="number" id="number-<?= $i ?>" ><?= $i ?></span></div><?php }
                 ?>
             </div>
             <div id="base">
@@ -14,19 +14,14 @@
                     <img src="/img/bombo.gif" />
                 </div>
                 <div id="ball-output">
-                        <div class="ball">
-                            8
-                        </div>
-                        <div class="ball">
-                            88
-                        </div>
+                        <div class="ball" style="display: none"></div>
                 </div>
                 <div id="controls">
-                    <a id="new-ball"  class="button">
+                    <a id="new-ball"  class="button"  href="#">
                         <span>BOLA</span>
                     </a>
                     <br />
-                    <a id="new-game" class="button">
+                    <a id="new-game" class="button" href="">
                         <span>Nueva Partida</span>
                     </a>
 
