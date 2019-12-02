4a1ae13b3d039d3e5ac1eaba83740ffecc2e9770
lorenzo-carruesco <lorenzo.carruesco@gmail.com>
2019-12-02 22:24:20 +0100

    script refactor

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
-$("#new-game").click(function(e) {
-    return confirm("Nueva Partida");
-});
-
-$("#new-ball").click(function(e) {
-    e.preventDefault();
-    var remaining_numbers = $(".number:not(.glowed)");
-    var index = Math.floor((Math.random() * remaining_numbers.size())); 
-    var number = remaining_numbers.eq(index).text();
-    $("#ball-output .ball").fadeOut(300, function () {
-        $("#ball-output").html('<div class="ball" style="display:none">'+number+'</div>');
-        $("#ball-output .ball").fadeIn(300, function () {
-            $(".number#number-"+number).addClass('glowed');
+var bingo = {
+    last_number: 0,
+    remaining_numbers: [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+        31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+        41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+        51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+        61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
+        71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+        81, 82, 83, 84, 85, 86, 87, 88, 89, 90
+    ],
+    getNewNumber: function () {
+        let index = Math.floor(Math.random() * bingo.remaining_numbers.length);
+        bingo.last_number = bingo.remaining_numbers[index];
+        bingo.remaining_numbers.filter(item => item !== bingo.last_number);
+    },
+    newBallClick: function (e) {
+        e.preventDefault();
+        bingo.getNewNumber();
+        bingo.printLastNumber();
+    },
+    printLastNumber: function () {
+        $("#ball-output .ball").fadeOut(300, function () {
+            $("#ball-output").html('<div class="ball" style="display:none">' + bingo.last_number + '</div>');
+            $("#ball-output .ball").fadeIn(300, function () {
+                $(".number#number-" + bingo.last_number).addClass('glowed');
+            });
         });
-    });
+    },
+    init: function () {
+
+        document.getElementById('new-ball').addEventListener("click", bingo.newBallClick, false);
+        $("#new-game").click(function (e) {
+            return confirm("Nueva Partida");
+        });
+    }
+};
+document.addEventListener("DOMContentLoaded", function () {
+    bingo.init();
 });
